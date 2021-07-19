2ec7c180e5b17a94476681cd4a409bd6202b876b
CARLOS SANTANA NUNES (CRGL-THIRDPARTY.COM) <carlos_nunes@crgl-thirdparty.com>
2021-07-19 19:15:14 -0300

    testeInit

---
+++ AspNetCoreDapperTests/Repositories/FleetRepositoryTests.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AspNetCoreDapperTests.Repositories
+{
+    [TestClass]
+    public class FleetRepositoryTests
+    {
+        [TestMethod]
+        public void testeInit()
+        {
+            Assert.IsNotNull(true);
+            Assert.IsTrue(true);
+        }
+    }
+}
